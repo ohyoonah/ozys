69e9773f967d1993f94959482779c20628ef39f6
ohyoonah <dhdbsdk3@naver.com>
2022-11-14 02:10:01 +0900

    드롭다운 메뉴 컴포넌트

---
+++ src/components/drop_data.ts
@@ -0,0 +1,62 @@
+export const dropData = [
+  {
+    token_image: "https://s.ozys.io/bridge/orbitBridge/icon/ORC.png",
+    token_name: "ORC",
+    token_price: 1.0001,
+  },
+  {
+    token_image: "https://s.ozys.io/bridge/orbitBridge/icon/ETH.png",
+    token_name: "ETH",
+    token_price: 1.002,
+  },
+  {
+    token_image: "https://s.ozys.io/bridge/orbitBridge/icon/USDC.png",
+    token_name: "USDC",
+    token_price: 1.003,
+  },
+  {
+    token_image: "https://s.ozys.io/bridge/orbitBridge/icon/USDT.png",
+    token_name: "USDT",
+    token_price: 1.004,
+  },
+  {
+    token_image: "https://s.ozys.io/bridge/orbitBridge/icon/WBTC.png",
+    token_name: "WBTC",
+    token_price: 1.005,
+  },
+  {
+    token_image: "https://s.ozys.io/bridge/orbitBridge/icon/MATIC.png",
+    token_name: "MATIC",
+    token_price: 1.006,
+  },
+  {
+    token_image: "https://s.ozys.io/bridge/orbitBridge/icon/MESH.png",
+    token_name: "MESH",
+    token_price: 1.007,
+  },
+  {
+    token_image: "https://s.ozys.io/bridge/orbitBridge/icon/XRP.png",
+    token_name: "XRP",
+    token_price: 1.008,
+  },
+  {
+    token_image: "https://s.ozys.io/bridge/orbitBridge/icon/DAI.png",
+    token_name: "DAI",
+    token_price: 1.009,
+  },
+  {
+    token_image: "https://s.ozys.io/bridge/orbitBridge/icon/KLAY.png",
+    token_name: "KLAY",
+    token_price: 1.01,
+  },
+  {
+    token_image: "https://s.ozys.io/bridge/orbitBridge/icon/KSP.png",
+    token_name: "ETH",
+    token_price: 1.011,
+  },
+  {
+    token_image: "https://s.ozys.io/bridge/orbitBridge/icon/AERGO.png",
+    token_name: "AERGO",
+    token_price: 1.012,
+  },
+];
